4e0655884dca9a373129911d37477d40f90b3077
Cantikaadyah <123210153@student.upnyk.ac.id>
2024-05-19 00:00:51 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+// Data user awal
+let user = {
+  Name          : "Cantika Dyah Wahyuningrum",
+  NIM           : "123210153",
+  Class         : "IF-C",
+  Major         : "Informatika",
+  Faculcities   : "FTI"
+};
+
+// Endpoint untuk mengembalikan user
+app.get('/', (req, res) => {
+  res.json(user);
+});
+
+// Endpoint untuk memperbarui user
+app.post('/', (req, res) => {
+  // Mendapatkan data user yang diperbarui dari body permintaan
+  const updateduser = req.body;
+
+  // Memperbarui user dengan data yang diterima
+  user = { ...user, ...updateduser };
+
+  res.json(user);
+});
+
+// Port yang digunakan oleh server
+const PORT = process.env.PORT || 8080;
+
+// Mulai server
+app.listen(PORT, () => {
+  console.log(`Server berjalan pada port ${PORT}`);
+});
