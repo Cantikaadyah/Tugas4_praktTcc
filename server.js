28742f10b245cd84bb0371f346505d12a1eff7e6
Cantikaadyah <123210153@student.upnyk.ac.id>
2024-05-19 00:57:43 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+// Data konser awal
+let concerts = [
+    {
+        nama: "Coldplay",
+        tanggal: "15 Agustus 2023",
+        lokasi: "Stadium Wembley, London",
+        genre: "Pop",
+        rating: 9.0
+    },
+    {
+        nama: "Ed Sheeran",
+        tanggal: "10 Juni 2022",
+        lokasi: "Madison Square Garden, New York",
+        genre: "Pop",
+        rating: 8.7
+    },
+];
+
+// Middleware untuk mengizinkan Express membaca body dari request
+app.use(express.json());
+
+// Endpoint untuk mengembalikan data konser
+app.get('/', (req, res) => {
+    res.json(concerts);
+});
+
+// Endpoint untuk memperbarui data konser
+app.post('/', (req, res) => {
+    // Mendapatkan data konser yang diperbarui dari body permintaan
+    const updatedConcerts = req.body;
+
+    // Memperbarui data konser dengan data yang diterima
+    concerts = [...concerts, ...updatedConcerts];
+
+    res.json(concerts);
+});
+
+// Port yang digunakan oleh server
+const PORT = process.env.PORT || 8080;
+
+// Mulai server
+app.listen(PORT, () => {
+    console.log(`Server berjalan pada port:${PORT}`);
+});
+
